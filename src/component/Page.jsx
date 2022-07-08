8aa48acf1391a8515e4c3db16cb4189a27a0e8d9
Fernanda Vidal <fevidal.dev@gmail.com>
2022-07-08 18:34:38 -0300

    Cria component Table e renderiza tabela. Req 1.

---
+++ src/component/Page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from './Table';
+
+function Page() {
+  return (
+    <div>
+      <span>Eu sou uma</span>
+      <Table />
+    </div>
+  );
+}
+
+export default Page;
---
+++ src/component/Table.jsx
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react';
+import { StarContext } from '../context/StarContext';
+// import fetchAPI from '../helpers/fetchApi';
+
+function Table() {
+  const { dataState } = useContext(StarContext);
+
+  return (
+    <div>
+      <span>tabela</span>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+        {dataState?.map((planet, i) => (
+          <tr key={ i + 1 }>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
+
+export default Table;
